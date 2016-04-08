32f953cdd83344236231a487476e98a13de8e2e4
yuan <chunhai.yuan@kangfuhui.com>
2016-04-08 11:11:13 +0800

    aa

--- cloud.js
+++ cloud.js
@@ -1,5 +1,6 @@
 var AV = require('leanengine');
 
+var Post = AV.Object.extend('Post');
 /**
  * 一个简单的云代码方法
  */
@@ -7,4 +8,20 @@ AV.Cloud.define('hello', function(request, response) {
   response.success('Hello world!');
 });
 
+AV.Cloud.define('_messageReceived', function(request, response) {
+  var params = request.params;
+
+  var post = new Post();
+  post.set('content', '每个 JavaScript 程序员必备的8个开发工具add by yuan');
+  post.set('pubUser', 'LeanCloud官方客服');
+  post.set('pubTimestamp', 1435541999);
+  post.save().then(function(post) {
+  // 成功保存之后，执行其他逻辑.
+    console.log('New object created with objectId: ' + post.id);
+  }, function(err) {
+  // 失败之后执行其他逻辑
+  // error 是 AV.Error 的实例，包含有错误码和描述信息.
+    console.log('Failed to create new object, with error message: ' + err.message);
+  });
+});
 module.exports = AV.Cloud;
