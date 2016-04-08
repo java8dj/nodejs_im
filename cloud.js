4d4aa55bcee0ad8c9769e70e30e462dffe29fcaf
java8dj <java8dj@163.com>
2016-04-08 14:21:27 +0800

    e

--- cloud.js
+++ cloud.js
@@ -9,50 +9,31 @@ AV.Cloud.define('hello', function(request, response) {
   response.success({'key':'Hello world!'});
 });
 
-//AV.Cloud.define("_messageReceived", function(request,response){
-    //console.log("messageReceived");
-//    response.success();
-//});
+AV.Cloud.define("_messageSent", function(request,response){
+    response.success();
+});
 AV.Cloud.define("_receiversOffline", function(request,response){
-    //console.log("recieverOffline");
     response.success();
 });
 AV.Cloud.define("_conversationAdd", function(request,response){
-    //console.log("_conversationAdd");
-    //console.log(request);
     response.success();
 });
 AV.Cloud.define("_conversationRemove", function(request,response){
-    //console.log("_conversationRemove");
-    //console.log(request);
     response.success();
 });
 AV.Cloud.define("_conversationStart", function(request,response){
-    //console.log("_conversationStart");
-    //console.log(request);
     response.success();
 });
-// _messageSent
 AV.Cloud.define("_messageReceived",function(request,response){
 
     console.log("messageSent");
-    //console.log(request);
     var content = request.params.content;
     var json = JSON.parse(content);
-    //console.log(content);
-    //console.log("json");
-    //console.log(json);
-    //var testObj = new TestObj();
-    //testObj.set("words",json._lctype+" 类型类型");
-    //testObj.save().then(function(post){
-    //    console.log("save-objId="+post.id);
-    //});
 
     var lctext = json._lctext;
     var lcattrs = json._lcattrs;
     var mentionUserIds = lcattrs.mentionUserIds;
     if(mentionUserIds){
-        //如果带有mentionUserId，表明是@功能
         var user = {};
         user.id = lcattrs.user.id;
         user.logo = lcattrs.user.logo;
